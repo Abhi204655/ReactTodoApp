ab3e9102c1bf909279510c5e1e703953c4418699
Abhi204655 <Abhi9780253712@gmail.com>
2020-04-28 13:38:08 +0530

    Updates

---
+++ precache-manifest.54dcb093e3e21b0f8d2800ef7ff27dac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63798b8c6ee378528896fffda451629",
+    "url": "/ReactTodoApp/index.html"
+  },
+  {
+    "revision": "26e3c7e9ff787c6dc57a",
+    "url": "/ReactTodoApp/static/css/main.b11ead82.chunk.css"
+  },
+  {
+    "revision": "95e5879bd2f1a838f19c",
+    "url": "/ReactTodoApp/static/js/2.e8d98b87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactTodoApp/static/js/2.e8d98b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e3c7e9ff787c6dc57a",
+    "url": "/ReactTodoApp/static/js/main.3c2093d7.chunk.js"
+  },
+  {
+    "revision": "b3be83a2a846439c32ba",
+    "url": "/ReactTodoApp/static/js/runtime-main.7fa7039b.js"
+  }
+]);
